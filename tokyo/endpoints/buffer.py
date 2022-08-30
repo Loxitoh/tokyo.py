9fe108d8e5f50244ddff0e1dc06fc105641b708e
Lox <101979581+Loxitoh@users.noreply.github.com>
2022-08-30 16:42:40 -0400

    Update buffer.py

--- tokyo/endpoints/buffer.py
+++ tokyo/endpoints/buffer.py
@@ -13,20 +13,20 @@ class Buffer:
         `Get the bytes response of the given endpoint`
     '''
 
-    async def get(endpoint: str = None, parameters: dict = None) -> aiohttp.ClientResponse:
+    async def get(endpoint: str = None, builders: dict = None) -> aiohttp.ClientResponse:
         '''Get the bytes response of the given endpoint'''
 
         dictionary = Api().endpoints
 
         if endpoint is None:
             raise TypeError('Parameters cannot be empty.')
-        elif parameters is None:
+        elif builders is None:
             raise TypeError('Parameters cannot be empty.')
-        elif not isinstance(parameters, dict):
+        elif not isinstance(builders, dict):
             raise TypeError('Innapropiate argument type (dict is required).')
         elif endpoint not in dictionary['BUFFER']:
             raise TypeError(f'Could not find endpoint "{endpoint}" in the buffer endpoints dictionary.')
 
         async with aiohttp.ClientSession() as session:
-            async with session.get(url = f'{Api().base}/image/{endpoint}', params = parameters) as response:
+            async with session.get(url = f'{Api().base}/image/{endpoint}', params = builders) as response:
                 return BytesIO(await response.read()).getvalue()
