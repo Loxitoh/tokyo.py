24f2f745b49d24ef1ea6d9501f52e2193571d52e
Lox <101979581+Loxitoh@users.noreply.github.com>
2022-08-31 16:00:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
 
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
 setup(
     name = 'tokyo.py',
     version = '0.0.1',
     description = 'Python library for interacting with Tokyo API (https://api.miduwu.ga/)',
-    url = 'https://github.com/Loxitoh/tokyo.py.git',
+    long_description = readme,
+    url = 'https://github.com/Loxitoh/tokyo.py',
     author = 'Loxitoh',
     author_email = 'loxitoh@gmail.com',
     license = 'MIT',
-    packages = ['tokyo'],
+    packages = [
+        'tokyo',
+        'tokyo.endpoints'
+    ],
+    include_package_data = True,
+    requires = requirements,
     zip_safe = False
 )
